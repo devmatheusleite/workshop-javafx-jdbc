f109ad00a57248694606d75012302bdc5295cfd6
Matheus Leite <maatheus2710@gmail.com>
2022-06-16 19:17:32 -0300

    Main view controller

--- src/module-info.java
+++ src/module-info.java
@@ -6,4 +6,16 @@ module workshop {
 
 	opens application to javafx.graphics, javafx.fxml;
 
+	opens gui to javafx.graphics, javafx.fxml;
+
+//	opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
+//
+//	opens model.services to javafx.graphics, javafx.fxml;
+//	
+//	opens gui.util to javafx.graphics, javafx.fxml;
+//	
+//	opens db to javafx.graphics, javafx.fxml;
+//	
+//	opens model.dao to javafx.graphics, javafx.fxml;
+
 }
