1595e8c5a19fee63cbad928f2622e25422293dbb
Matheus Leite <maatheus2710@gmail.com>
2022-06-17 17:47:33 -0300

    DepartmentFormController

--- src/module-info.java
+++ src/module-info.java
@@ -5,17 +5,19 @@ module workshop {
 	requires java.sql;
 
 	opens application to javafx.graphics, javafx.fxml;
+	
+	opens db to javafx.graphics, javafx.fxml;
 
 	opens gui to javafx.graphics, javafx.fxml;
 
 	opens gui.util to javafx.graphics, javafx.fxml;
+	
+	opens model.dao to javafx.graphics, javafx.fxml;
+	
+	opens model.dao.impl to javafx.graphics, javafx.fxml;
 
 	opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
 	
 	opens model.services to javafx.graphics, javafx.fxml;
-	
-	opens model.dao to javafx.graphics, javafx.fxml;
-	
-	opens db to javafx.graphics, javafx.fxml;
 	
 }
